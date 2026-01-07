5479314d83140857297a8216cde122b825185a51
Khadija Daruwala <khadija.genius@gmail.com>
2026-01-07 14:22:05 -0500

    Optimized solution for time complexity

--- Array/1-Two-sum.swift
+++ Array/1-Two-sum.swift
@@ -1,5 +1,26 @@
-/* Solution 1*/
+/* Solution 1 -> Brute Force (Time Complexity= O(n^2)) */
 class Solution1 {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var index1 = 0
+        var index2 = 1
+        
+        while index1 != index2 {
+            while index2 < nums.count {
+                if nums[index1] + nums [index2] == target {
+                    return [index1, index2]
+                } else {
+                    index2 += 1
+                }
+            }
+            index1 += 1
+            index2 = index1 + 1
+        }
+        return [index1, index2]
+    }
+}
+
+/* Solution 2 (Time Complexity= O(n^2)) */
+class Solution2 {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var result = [Int]()
         var dict = [Int : Int]()
@@ -15,19 +36,17 @@ class Solution1 {
     }
 }
 
-/* Solution 2*/
-class Solution2 {
+/* Solution 3 (Time Complexity= O(n))*/
+class Solution3 {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dict = [Int: Int]()
         
-        for (i, num) in nums.enumerated() {
-            if let index = dict[target - num] {
-                if i != index {
-                    return [index, i]
-                }
+        for i in 0..<nums.count {
+            if let index = dict[nums[i]] {
+                return [index, i]
             }
-            dict[num] = i
+            dict[target - nums[i]] = i
         }
-        return[0]
+        return []
     }
 }
